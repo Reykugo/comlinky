b492ef9e9aa0865776eecd0f7db38209c547a818
Guillaume <guillaume.nougier@ynov.com>
2018-02-09 17:13:06 +0100

    debut process user service

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+import {UserService} from './services/user.service';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  users: Array<any>;
+  requesting : boolean;
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(){
+    this.requesting = false;
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.requesting = true;
+    this.userService.getUsers().subscribe((res) =>{
+      this.users = res.users;
+      this.requesting = false
+    }, (err) =>{
+      this.requesting = false;
+    })
+  }
 }
